e70ba4f4a47692deb3d36c6ae0a459fdc959020f
Beniamin-Emanuel Imbrescu <160716189+Stikeez14@users.noreply.github.com>
2024-03-30 23:54:33 +0200

    S1: Second Commit -> Function for reading the directory and his sub_directories recursively (Also adding each file path and name in the Snapshot.txt file)  &  Renamed some variables for better readability

--- s1.c
+++ s1.c
@@ -1,38 +1,59 @@
-//S1 Description: The proposed project combines functionalities for monitoring a directory to manage 
-//differences between two captures (snapshots) of it. The user will be able to observe and intervene 
-//in the changes in the monitored directory.
-
-//Directory Monitoring: The user can specify the directory to be monitored as an argument in the command 
-//line, and the program will track changes occurring in it and its subdirectories, parsing recursively each 
-//entry from the directory. With each run of the program, the snapshot of the directory will be updated, 
-//storing the metadata of each entry.
-
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int main(int argc,char *argv[]){
-
-    DIR *d;
-    struct dirent *dir;
-    if(argc==2){
-        char *path=argv[1];
-        d=opendir(path);
-        if(d){
-            while((dir=readdir(d))!=NULL){
-                printf("%s\n",dir->d_name);   //printing the content of the directory
+void read_directories(const char *path, FILE *fp){
+    DIR *d = opendir(path);
+    struct dirent *dir_file;
+    if(d){
+        while((dir_file = readdir(d)) != NULL){
+            if(strcmp(dir_file->d_name, ".") == 0 || strcmp(dir_file->d_name, "..") == 0){
+                continue; //all the directories have the entries "." & ".." 
+                          //i'm not printing them in the Snapshot file. 
             }
-            closedir(d);
+            char new_path[1024]; //concatenating the path and the name of each entry into 
+                                 // 'new_path'
+            snprintf(new_path, sizeof(new_path), "%s/%s", path, dir_file->d_name);
+
+            struct stat st;                            
+            if (lstat(new_path, &st) == -1) { //get file information with lstat
+                perror("error");                    //& print error message in case of failing
+                continue;
+            }
+            fprintf(fp, "%s\n", new_path); // printing every snapshot_entry in the Snapshot file
+            if(S_ISDIR(st.st_mode)){     //if an entry is a directory
+                                         //recursively call again the function with the new path     
+                read_directories(new_path, fp);
+            }
         }
-        else{
-            printf("error: The given path is incorrect!\n"); //if the path to the directory is incorrect =>
-                                                             // => error message + exits the program
-            exit(EXIT_FAILURE);                     
-        } 
+        closedir(d);
     }
     else{
-        printf("error: There should be only one argument!\n");  //if in the terminal is not exactly one argument
-        exit(EXIT_FAILURE);                                     // (the path to the directory) => error mesage + exits the program
+        printf("error: The given path is incorrect!\n");
+        exit(EXIT_FAILURE);
+    } 
+}
+
+int main(int argc, char *argv[]){
+    
+    FILE *fp = fopen("Snapshot.txt", "w");
+
+    if(fp == NULL){
+        printf("error: Cannot open the snapshot file!\n");
+        exit(EXIT_FAILURE);        
     }
+    if(argc == 2){
+        char *path = argv[1];
+        read_directories(path, fp);
+    }
+    else{
+        printf("error: There should be only one argument!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(fp);
+    return 0;
 }
