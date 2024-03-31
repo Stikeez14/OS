f0dfc5068360859ea2316828afcbc1acf885b4a6
Beniamin-Emanuel Imbrescu <160716189+Stikeez14@users.noreply.github.com>
2024-03-31 14:52:35 +0300

    S1: Fourth Commit -> Additional error handling (also using fprintf to print errors to stderr) & Allocating memory outside the while loop (and reallocating in the loop)

--- s1.c
+++ s1.c
@@ -1,29 +1,41 @@
+//S1 Description: The proposed project combines functionalities for monitoring a directory to manage 
+//differences between two captures (snapshots) of it. The user will be able to observe and intervene 
+//in the changes in the monitored directory.
+
+//Directory Monitoring: The user can specify the directory to be monitored as an argument in the command 
+//line, and the program will track changes occurring in it and its subdirectories, parsing recursively each 
+//entry from the directory. With each run of the program, the snapshot of the directory will be updated, 
+//storing the metadata of each entry.
+
+
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <errno.h>
 
 void read_directories(const char *path, FILE *fp){
     DIR *d = opendir(path);
     struct dirent *dir_file;
+    char *new_path=NULL;
     if(d){
         while((dir_file = readdir(d)) != NULL){
             if(strcmp(dir_file->d_name, ".") == 0 || strcmp(dir_file->d_name, "..") == 0){
                 continue; //all the directories have the entries "." & ".." 
                           //i'm not printing them in the Snapshot file. 
             }
-            char *new_path=malloc(strlen(path)+strlen(dir_file->d_name) +2);
-            if(new_path==NULL){
-                perror("error: Failed to allocate memory");
+            new_path=realloc(new_path,strlen(path)+strlen(dir_file->d_name) +2); //+2 is for '/' and null terminator
+            if(new_path==NULL){                                             
+                fprintf(stderr,"error: Failed to allocate memory for path: %s\n",dir_file->d_name);
                 exit(EXIT_FAILURE);
             }
             sprintf(new_path,"%s/%s", path, dir_file->d_name);
 
             struct stat st;                            
-            if (lstat(new_path, &st) == -1) { //get file information with lstat
-                perror("error");              //& print error message in case of failing
+            if (lstat(new_path, &st) == -1) {                                             //get file information with lstat
+                fprintf(stderr,"error: Failed to get information for: %s\n",new_path);    //& print error message in case of failing
                 free(new_path);                  
                 continue;
             }
@@ -32,12 +44,12 @@ void read_directories(const char *path, FILE *fp){
                                            //recursively call again the function with the new path     
                 read_directories(new_path, fp);
             }
-            free(new_path);
         }
+        free(new_path);
         closedir(d);
     }
     else{
-        printf("error: The given path is incorrect!\n");
+        fprintf(stderr,"error: The given path is incorrect!\n");
         exit(EXIT_FAILURE);
     } 
 }
@@ -47,7 +59,7 @@ int main(int argc, char *argv[]){
     FILE *fp = fopen("Snapshot.txt", "w");
 
     if(fp == NULL){
-        printf("error: Cannot open the snapshot file!\n");
+        fprintf(stderr,"error: Cannot open the snapshot file!\n");
         exit(EXIT_FAILURE);        
     }
     if(argc == 2){
@@ -55,7 +67,7 @@ int main(int argc, char *argv[]){
         read_directories(path, fp);
     }
     else{
-        printf("error: There should be only one argument!\n");
+        fprintf(stderr,"error: There should be only one argument!\n");
         exit(EXIT_FAILURE);
     }
 
