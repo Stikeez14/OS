2a8fb0b521f05a089032d0f608fe143449146e20
Beniamin-Emanuel Imbrescu <160716189+Stikeez14@users.noreply.github.com>
2024-04-08 22:44:26 +0300

    S2: Tenth Commit -> Reorganized the code beacause it was hard to read and understant what's happening due to so many indentations in some functions

--- s2.c
+++ s2.c
@@ -29,17 +29,37 @@
 #include <linux/limits.h>
 #include <libgen.h>
 
-
-//used mostly for error handling and printing messages
-void write_message(const char *message,const char* dir_name){
-    write(STDERR_FILENO,message,strlen(message));
-    write(STDERR_FILENO,dir_name,strlen(dir_name));
-    write(STDERR_FILENO,"\n",1);
-}
-
+/*
+    FUNCTION PROTOTYPES
+*/
 
 //traverse the directory given as argument and his sub_directories recursively
 //and writes in the snapshot file the path and the name of each entry 
+void read_directories(const char *path, int snapshot_fd);
+
+//creates a snapshot file in the output directory for each 
+//directory given as argument in the terminal for monitoring.
+void create_snapshot(char *path, char *output_path);
+
+//counts the existing snapshots created for each monitored directory
+//from the output directory. If no snapshot is found for one of the directories, it will return 0
+int count_snapshots(const char *output_path,const char *dir_name);
+
+//searches in the output directory all the entries for the an monitored directory and calls 
+//the compare_snapshots function if two snapshots are found. If there is only one, no comparation is made
+void get_previous_snapshot(const char *output_path, const char *dir_name,const char *snapshot_file_name, int snp_no);
+
+//used mostly for error handling and printing messages
+void write_message(const char *message,const char* dir_name);
+
+//compares the snapshot that is created when the program runs, with the snapshot that was previously created
+//then overrides the previous snapshot with the information of the current
+void compare_snapshots(const char *prev_snapshot_file_name,const char *snapshot_file_name,const char *dir_name);
+
+
+/*
+    READ DIRECTORIES FUNCTION IMPLEMENTATION
+*/
 void read_directories(const char *path, int snapshot_fd){
 
     DIR *d = opendir(path);
@@ -92,134 +112,9 @@ void read_directories(const char *path, int snapshot_fd){
 }
 
 
-//counts the existing snapshots created for each monitored directory
-//from the output directory. If no snapshot is found for one of the directories, it will return 0
-int count_snapshots(const char *output_path,const char *dir_name){
-    
-    int count=0;
-
-    DIR *d=opendir(output_path);
-    struct dirent *dir_entry;
-
-    if(d){
-        while((dir_entry=readdir(d))!=NULL){ 
-
-           if(strstr(dir_entry->d_name, dir_name) == dir_entry->d_name &&  //cheks if the dir_name matches the beggining
-                strstr(dir_entry->d_name, "_Snapshot(") != NULL) {         //of the snapshot file name & if it contains the substring 
-                count++;
-            }
-        }
-        closedir(d);
-    }
-    else{
-        write_message("*count_snapshots* error: Failed to open the directory ",dir_name);
-    }
-    return count;
-}
-
-//compares the snapshot that is created when the program runs, with the snapshot that was created before
-//overrides the previous snapshot with the information of the current
-void compare_snapshots(const char *output_path, const char *dir_name,const char *snapshot_file_name, int snp_no){
-
-    DIR *d=opendir(output_path);
-    struct dirent *dir_entry;
-                                           
-    if(d){
-        int prev_snapshot_no=0;
-        char prev_snapshot_file_name[FILENAME_MAX];
-
-        //parsing through all the snapshot files from the output that starts with
-        //directory name that is monitored
-        while((dir_entry=readdir(d))!=NULL){
-            if(strstr(dir_entry->d_name,dir_name)==dir_entry->d_name && strstr(dir_entry->d_name,"_Snapshot(")!=NULL){
-                char *snp_no_str=strtok(dir_entry->d_name,"(");    //tokenized the name for obtaining the substring
-                snp_no_str=strtok(NULL,"(");                       //containing the snapshot number (ex --> (2) )
-                prev_snapshot_no=atoi(snp_no_str);   //convert the string to int
-
-                if(prev_snapshot_no<snp_no){
-                    strcpy(prev_snapshot_file_name,output_path);    //--> constructing the name of the previous
-                    strcat(prev_snapshot_file_name,"/");            //    snapshot file
-                    strcat(prev_snapshot_file_name,dir_entry->d_name);
-                    strcat(prev_snapshot_file_name,"(");
-                    strcat(prev_snapshot_file_name,snp_no_str);
-                } 
-            }
-        }
-    
-        if(prev_snapshot_no<1){  //if in the folder is not a previous snapshot => no comparison will be made
-            write_message("--> no snapshots were previously created for ",dir_name);
-        }
-        else{
-            int snapshot_fd_current = open(snapshot_file_name, O_RDONLY, S_IRUSR); //opening the current snapshot file in reading mode
-            if(snapshot_fd_current == -1){
-                write_message("*compare_snapshots* error: Failed to open the current snapshot file for ",dir_name);
-                return;
-            }
-
-            int snapshot_fd_prev = open(prev_snapshot_file_name, O_RDWR, S_IRUSR); //opening the previous snapshot file in reading 
-                                                                                   //and writing mode
-            if(snapshot_fd_prev == -1){
-                write_message("*compare_snapshots* error: Failed to open the previous snapshot file for ",dir_name);
-                return;
-            }
-
-            char current_line[256];  //will store the lines into these two bubffers
-            char prev_line[256];
-            int line=1;
-            int IsDifferent=0;
-
-            while(1){
-
-                ssize_t current_read=read(snapshot_fd_current, current_line, sizeof(current_line)-1); //reading data from the snapshot 
-                ssize_t prev_read=read(snapshot_fd_prev, prev_line, sizeof(prev_line)-1);             //file descriptors into the buffers 
-
-                if(current_read==0 && prev_read==0){ //files reached EOF
-                    break;
-                }
-                if(current_read==0 || prev_read==0){ //if one of the files has less lines
-                                                     // => the files are different
-                    if(current_read==0){
-                        lseek(snapshot_fd_current, -prev_read, SEEK_CUR);  //positions the file pointer at the beginning of the line
-                                                                           // in the prev snapshot
-                        while(read(snapshot_fd_prev, prev_line, sizeof(prev_line)-1)>0){
-                            write(snapshot_fd_current, prev_line, strlen(prev_line)); 
-                            }                                                                     // --> write the lines of the fie that
-                    } else {                                                                      // has more in the prev snapshot
-                        while(read(snapshot_fd_current, current_line, sizeof(current_line)-1)>0){
-                            write(snapshot_fd_prev, current_line, strlen(current_line));
-                        }
-                    }               //REVENIM CU DOUA BUFERE 
-                    IsDifferent=1;
-                }
-
-                if(strcmp(current_line, prev_line)!=0){   //if the current lines are not equal => files are diff
-                                                          // --> will override the previous snapshot
-                    lseek(snapshot_fd_prev, -prev_read, SEEK_CUR);  //
-                    write(snapshot_fd_prev, current_line, strlen(current_line)); //writing in the previous snapshot
-                    IsDifferent = 1;                                             //the line from the current snapshot
-                }
-
-                line++;
-            }
-
-            if(!IsDifferent){   //in case no difference is found
-                write(STDERR_FILENO,"No differences found in the snapshots!\n",
-                  strlen("No differences found in the snapshots!\n"));
-            }
-
-            close(snapshot_fd_current);
-            close(snapshot_fd_prev);
-        }
-        closedir(d);
-    }
-    else{
-        write_message("*compare_snapshots* error: Failed to open the output directory ",dir_name);
-    }
-}
-
-
-//creates a snapshot file in the output directory for each 
-//directory given as argument in the terminal for monitoring
+/*
+    CREATE SNAPSHOT FUNCTION IMPLEMENTATION
+*/
 void create_snapshot(char *path, char *output_path){
 
     char *dir_name=basename((char *)path);  //gets the name of the input directory
@@ -272,7 +167,155 @@ void create_snapshot(char *path, char *output_path){
     // (MAYBE AN EXECUTION TIME TO SEE HOW MUCH IT TAKES OR AN PROGRESSION BAR IN THE FUTURE)
 
     close(snapshot_fd);
-    compare_snapshots(output_path,dir_name,snapshot_file_name,snapshots_no);
+    get_previous_snapshot(output_path,dir_name,snapshot_file_name,snapshots_no);
+}
+
+
+/*
+    COUNT SNAPSHOTS FUNCTION IMPLEMENTATION
+*/
+int count_snapshots(const char *output_path,const char *dir_name){
+    
+    int count=0;
+
+    DIR *d=opendir(output_path);
+    struct dirent *dir_entry;
+
+    if(d){
+        while((dir_entry=readdir(d))!=NULL){ 
+
+           if(strstr(dir_entry->d_name, dir_name) == dir_entry->d_name &&  //cheks if the dir_name matches the beggining
+                strstr(dir_entry->d_name, "_Snapshot(") != NULL) {         //of the snapshot file name & if it contains the substring 
+                count++;
+            }
+        }
+        closedir(d);
+    }
+    else{
+        write_message("*count_snapshots* error: Failed to open the directory ",dir_name);
+    }
+    return count;
+}
+
+
+/*
+    GET PREVIOUS SNAPSHOT FUNCTION IMPLEMENTATION
+*/
+void get_previous_snapshot(const char *output_path, const char *dir_name,const char *snapshot_file_name, int snp_no){
+
+    DIR *d=opendir(output_path);
+    struct dirent *dir_entry;
+                                           
+    if(d){
+        int prev_snapshot_no=0;
+        char prev_snapshot_file_name[FILENAME_MAX];
+
+        //parsing through all the snapshot files from the output that starts with
+        //directory name that is monitored
+        while((dir_entry=readdir(d))!=NULL){
+            if(strstr(dir_entry->d_name,dir_name)==dir_entry->d_name && strstr(dir_entry->d_name,"_Snapshot(")!=NULL){
+                char *snp_no_str=strtok(dir_entry->d_name,"(");    //tokenized the name for obtaining the substring
+                snp_no_str=strtok(NULL,"(");                       //containing the snapshot number (ex --> (2) )
+                prev_snapshot_no=atoi(snp_no_str);   //convert the string to int
+
+                if(prev_snapshot_no<snp_no){
+                    strcpy(prev_snapshot_file_name,output_path);    //--> constructing the name of the previous
+                    strcat(prev_snapshot_file_name,"/");            //    snapshot file
+                    strcat(prev_snapshot_file_name,dir_entry->d_name);
+                    strcat(prev_snapshot_file_name,"(");
+                    strcat(prev_snapshot_file_name,snp_no_str);
+                } 
+            }
+        }
+    
+        if(prev_snapshot_no<1){  //if in the folder is not a previous snapshot => no comparison will be made
+            write_message("--> no snapshots were previously created for ",dir_name);
+        }
+        else{
+            compare_snapshots(prev_snapshot_file_name,snapshot_file_name,dir_name);
+        }
+    }
+    else{
+        write_message("*compare_snapshots* error: Failed to open the output directory ",dir_name);
+    }
+    closedir(d);
+}
+
+
+/*
+    COMPARE SNAPSHOTS FUNCTION IMPLEMENTATION
+*/
+void compare_snapshots(const char *prev_snapshot_file_name,const char *snapshot_file_name,const char *dir_name){
+
+    int snapshot_fd_current = open(snapshot_file_name, O_RDONLY, S_IRUSR); //opening the current snapshot file in reading mode
+    if(snapshot_fd_current == -1){
+        write_message("*compare_snapshots* error: Failed to open the current snapshot file for ",dir_name);
+        return;
+    }
+
+    int snapshot_fd_prev = open(prev_snapshot_file_name, O_RDWR, S_IRUSR); //opening the previous snapshot file in reading 
+                                                                               //and writing mode
+    if(snapshot_fd_prev == -1){
+        write_message("*compare_snapshots* error: Failed to open the previous snapshot file for ",dir_name);
+        return;
+    }
+
+    char current_line[256];  //will store the lines into these two bubffers
+    char prev_line[256];
+    int line=1;
+    int IsDifferent=0;
+
+    while(1){
+
+        ssize_t current_read=read(snapshot_fd_current, current_line, sizeof(current_line)-1); //reading data from the snapshot 
+        ssize_t prev_read=read(snapshot_fd_prev, prev_line, sizeof(prev_line)-1);             //file descriptors into the buffers 
+
+        if(current_read==0 && prev_read==0){ //files reached EOF
+            break;
+        }
+        if(current_read==0 || prev_read==0){ //if one of the files has less lines
+                                                 // => the files are different
+        if(current_read==0){
+            lseek(snapshot_fd_current, -prev_read, SEEK_CUR);  //positions the file pointer at the beginning of the line
+                                                               // in the prev snapshot
+                while(read(snapshot_fd_prev, prev_line, sizeof(prev_line)-1)>0){
+                    write(snapshot_fd_current, prev_line, strlen(prev_line)); 
+                }                                                                     // --> write the lines of the fie that
+            } else {                                                                      // has more in the prev snapshot
+                while(read(snapshot_fd_current, current_line, sizeof(current_line)-1)>0){
+                    write(snapshot_fd_prev, current_line, strlen(current_line));
+                }
+            }               //REVENIM CU DOUA BUFERE 
+            IsDifferent=1;
+        }
+
+        if(strcmp(current_line, prev_line)!=0){   //if the current lines are not equal => files are diff
+                                                  // --> will override the previous snapshot
+            lseek(snapshot_fd_prev, -prev_read, SEEK_CUR);  //
+            write(snapshot_fd_prev, current_line, strlen(current_line)); //writing in the previous snapshot
+            IsDifferent = 1;                                             //the line from the current snapshot
+        }
+
+        line++;
+    }
+
+    if(!IsDifferent){   //in case no difference is found
+        write(STDERR_FILENO,"No differences found in the snapshots!\n",
+          strlen("No differences found in the snapshots!\n"));
+    }
+
+    close(snapshot_fd_current);
+    close(snapshot_fd_prev);
+}
+
+
+/*
+    WRITE MESSAGE FUNCTION IMPLEMENTATION
+*/
+void write_message(const char *message,const char* dir_name){
+    write(STDERR_FILENO,message,strlen(message));
+    write(STDERR_FILENO,dir_name,strlen(dir_name));
+    write(STDERR_FILENO,"\n",1);
 }
 
 
